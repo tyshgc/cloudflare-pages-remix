2b4d4945af21f519aa25a8026dce1c6a9d7570b0
Tsuyoshi Higuchi <hello@tyshgc.pw>
2024-11-17 09:10:07 +0900

    fix: configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+} from "@remix-run/dev"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 declare module "@remix-run/cloudflare" {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
@@ -25,4 +25,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+})
